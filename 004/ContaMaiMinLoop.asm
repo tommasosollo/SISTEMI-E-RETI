06ba147cc546e44d0b49a00f805dabff638d2a6f
tommasosollo <114075798+tommasosollo@users.noreply.github.com>
2023-01-19 08:27:15 +0100

    Create ContaMaiMinLoop.asm

---
+++ 004/ContaMaiMinLoop.asm
@@ -0,0 +1,114 @@
+.model small
+
+.stack 100h
+
+.data
+    
+    car db ?  
+    
+    NMAI db 30h
+    NMIN db 30h
+    NC db 30h         ;contatori cicli
+    
+    input db 'inserisci un carattere: ', '$'
+    outputMai db 'caratteri maiuscoli: ', '$'
+    outputMin db 'caratteri minuscoli: ', '$'
+    acapo db ,13,10,'$'
+    
+.code
+    .startup 
+
+        mov ax, @data     ;caricamento segmento dati in ds
+        mov ds, ax
+
+
+        inizio:
+        
+             
+
+            mov ah, 09h
+            mov dx, offset input    ;visualizzaione stringa di input
+            int 21h 
+            
+            mov ah, 01h             
+            int 21h 
+            mov car, al                ;richiesta input 
+            
+            mov ah,09h
+            mov dx, offset acapo               ;formattazione
+            int 21h 
+            
+            mov al, car 
+            
+            
+            cmp al, ' '
+            je FINTUT
+
+
+            cmp al, 'A'                            ;|
+            jge VeroA                              ;| 
+            jmp fine                               ;|
+                                                   ;|
+            VeroA:                                 ;|
+                cmp al, 'Z'                        ;|
+                jle VeroB                          ;|
+                jmp FalsoB                         ;|
+                                                   ;|
+            VeroB:                                 ;|
+                inc NMAI                           ;|
+                jmp fine                           ;|
+                                                   ;|
+            FalsoB:                                ;|
+                cmp al, 'a'                        ;|
+                jge VeroC                          ;|
+                jmp fine                           ;|
+                                                   ;|
+            VeroC:                                 ;|
+                cmp al, 'z'                        ;|
+                jle VeroD                          ;|
+                jmp fine                           ;|
+                                                   ;|
+            VeroD:                                 ;|
+                inc NMIN                           ;|
+                jmp fine                           ;|
+                                                   ;|
+            fine:                                  ;|
+                inc NC                             ;|    
+            
+            
+            jmp inizio 
+            
+            
+            
+        FINTUT:
+            nop  
+            
+            
+        
+        mov ah,09h
+        mov dx, offset acapo               ;formattazione
+        int 21h         
+        
+        mov ah, 09h
+        mov dx, offset outputMai           ;visualizzaione stringa di output maiuscole
+        int 21h
+        
+        mov ah, 02h
+        mov dl, NMAI                       ;visualizzazione contatore maiuscole
+        int 21h
+        
+        mov ah, 09h
+        mov dx, offset acapo               ;formattazione
+        int 21h
+        
+        mov ah, 09h
+        mov dx, offset outputMin           ;visualizzaione stringa di output minuscole
+        int 21h
+        
+        mov ah, 02h                        ;visualizzazione contatore minuscole
+        mov dl, NMIN
+        int 21h    
+        
+        
+        mov ah, 4ch
+        int 21h
