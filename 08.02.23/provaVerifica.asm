6e5fa0cf3360e7715e5def08fb30860559a618cb
tommasosollo <114075798+tommasosollo@users.noreply.github.com>
2023-02-08 09:52:41 +0100

    Update provaVerifica.asm

--- 08.02.23/provaVerifica.asm
+++ 08.02.23/provaVerifica.asm
@@ -4,10 +4,10 @@
 
 .data
     
-    car db 30h  
+    car db ?  
     
-    spiega db 'scrivi una parola e premi spazio quando la parola e finita', '$'
-    input db 'inserisci una parola: ', '$'  
+    spiega db 'scrivi una frase e premi invio quando la frase e finita', '$'
+    input db 'inserisci una frase: ', '$'  
     acapo db 13,10,'$'
     
 .code
@@ -17,21 +17,21 @@
         mov ds, ax
               
         mov ah, 09h
-        mov dx, offset spiega    ;visualizzaione stringa di input
+        mov dx, offset spiega    ;visualizzazione stringa con spiegaione programma
         int 21h
         
         
         mov ah, 09h
-        mov dx, offset acapo    ;visualizzaione acapo
+        mov dx, offset acapo    ;visualizzazione acapo
         int 21h  
         
         mov ah, 09h
-        mov dx, offset acapo    ;visualizzaione acapo
+        mov dx, offset acapo    ;visualizzazione acapo
         int 21h        
         
         
         mov ah, 09h
-        mov dx, offset input    ;visualizzaione stringa di input
+        mov dx, offset input    ;visualizzazione stringa di input
         int 21h 
             
 
@@ -40,21 +40,22 @@
         inizio: 
             mov ah, 01h             
             int 21h 
-            mov car, al                ;richiesta input 
-            
-            mov al, car 
+            mov car, al                ;richiesta input  
             
             
-            cmp al, ' '
+            cmp al, 013               ;se l'utente inserisce spazio il programma si ferma
             je FINTUT
-
-
             jmp inizio 
             
             
             
         FINTUT:
-            nop  
+            nop 
+        
+        mov ah, 4ch
+        int 21h   
+        
+        end
             
             
      
